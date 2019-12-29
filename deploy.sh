e5be2f52e126db24db194fb89213a2867920c010
CIOBOTARU Ciprian <Ciprian.CIOBOTARU@sodexo.com>
2019-12-29 13:21:45 +0200

    fix 100

--- deploy.sh
+++ deploy.sh
@@ -1,18 +1,18 @@
 docker build -t ciprianciobotaru/multi-client:latest -t ciprianciobotaru/multi-client:$SHA -f ./client/Dockerfile ./client
 docker build -t ciprianciobotaru/multi-server:latest -t ciprianciobotaru/multi-server:$SHA -f ./server/Dockerfile ./server
 docker build -t ciprianciobotaru/multi-worker:latest -t ciprianciobotaru/multi-worker:$SHA -f ./worker/Dockerfile ./worker
-#docker build -t ciprianciobotaru/demoapi:latest -t ciprianciobotaru/demoapi:$SHA -f ./demoapi/Dockerfile ./demoapi
+docker build -t ciprianciobotaru/demoapi:latest -t ciprianciobotaru/demoapi:$SHA -f ./demoapi/Dockerfile ./demoapi
 
 docker push ciprianciobotaru/multi-client:latest
 docker push ciprianciobotaru/multi-server:latest
 docker push ciprianciobotaru/multi-worker:latest
-#docker push ciprianciobotaru/demoapi:latest
+docker push ciprianciobotaru/demoapi:latest
 
 
 docker push ciprianciobotaru/multi-client:$SHA
 docker push ciprianciobotaru/multi-server:$SHA
 docker push ciprianciobotaru/multi-worker:$SHA
-#docker push ciprianciobotaru/demoapi:latest:$SHA
+docker push ciprianciobotaru/demoapi:latest:$SHA
 
 kubectl apply -f k8s
 kubectl set image deployments/server-deployment server=ciprianciobotaru/multi-server:$SHA
