0b10b537fe1363c22148a2ddf3cb7294f27aac4c
CIOBOTARU Ciprian <Ciprian.CIOBOTARU@sodexo.com>
2019-12-29 15:44:02 +0200

    last cloud try

--- deploy.sh
+++ deploy.sh
@@ -1,21 +1,22 @@
-docker build -t ciprianciobotaru/multi-client:latest -t ciprianciobotaru/multi-client:$SHA -f ./client/Dockerfile ./client
-docker build -t ciprianciobotaru/multi-server:latest -t ciprianciobotaru/multi-server:$SHA -f ./server/Dockerfile ./server
-docker build -t ciprianciobotaru/multi-worker:latest -t ciprianciobotaru/multi-worker:$SHA -f ./worker/Dockerfile ./worker
-docker build -t ciprianciobotaru/demoapi:latest -t ciprianciobotaru/demoapi:$SHA -f ./demoapi/Dockerfile ./demoapi
+#docker build -t ciprianciobotaru/multi-client:latest -t ciprianciobotaru/multi-client:$SHA -f ./client/Dockerfile ./client
+#docker build -t ciprianciobotaru/multi-server:latest -t ciprianciobotaru/multi-server:$SHA -f ./server/Dockerfile ./server
+#docker build -t ciprianciobotaru/multi-worker:latest -t ciprianciobotaru/multi-worker:$SHA -f ./worker/Dockerfile ./worker
+#docker build -t ciprianciobotaru/demoapi:latest -t ciprianciobotaru/demoapi:$SHA -f ./demoapi/Dockerfile ./demoapi
 
-docker push ciprianciobotaru/multi-client:latest
-docker push ciprianciobotaru/multi-server:latest
-docker push ciprianciobotaru/multi-worker:latest
-docker push ciprianciobotaru/demoapi:latest
+#docker push ciprianciobotaru/multi-client:latest
+#docker push ciprianciobotaru/multi-server:latest
+#docker push ciprianciobotaru/multi-worker:latest
+#docker push ciprianciobotaru/demoapi:latest
 
 
-docker push ciprianciobotaru/multi-client:$SHA
-docker push ciprianciobotaru/multi-server:$SHA
-docker push ciprianciobotaru/multi-worker:$SHA
-docker push ciprianciobotaru/demoapi:latest:$SHA
+#docker push ciprianciobotaru/multi-client:$SHA
+#docker push ciprianciobotaru/multi-server:$SHA
+#docker push ciprianciobotaru/multi-worker:$SHA
+#docker push ciprianciobotaru/demoapi:latest:$SHA
 
+kubectl delete -f k8s
 kubectl apply -f k8s
-kubectl set image deployments/server-deployment server=ciprianciobotaru/multi-server:$SHA
-kubectl set image deployments/client-deployment client=ciprianciobotaru/multi-client:$SHA
-kubectl set image deployments/worker-deployment worker=ciprianciobotaru/multi-worker:$SHA
-kubectl set image deployments/demoapi-deployment demoapi=ciprianciobotaru/demoapi:$SHA
+#kubectl set image deployments/server-deployment server=ciprianciobotaru/multi-server:$SHA
+#kubectl set image deployments/client-deployment client=ciprianciobotaru/multi-client:$SHA
+#kubectl set image deployments/worker-deployment worker=ciprianciobotaru/multi-worker:$SHA
+#kubectl set image deployments/demoapi-deployment demoapi=ciprianciobotaru/demoapi:$SHA
